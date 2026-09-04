5d7571ff0eec1e10f9f9bf73144b35a890569bf0
soenneker <jake@soenneker.com>
2026-09-03 22:01:43 -0500

    Add interface inheritdoc references

--- src/Soenneker.Runners.ZipCode/Utils/ExcelFileReaderUtil.cs
+++ src/Soenneker.Runners.ZipCode/Utils/ExcelFileReaderUtil.cs
@@ -12,6 +12,7 @@ using Soenneker.Utils.Path.Abstract;
 
 namespace Soenneker.Runners.ZipCode.Utils;
 
+/// <inheritdoc cref="IExcelFileReaderUtil" />
 public sealed class ExcelFileReaderUtil : IExcelFileReaderUtil
 {
     private readonly ILogger<ExcelFileReaderUtil> _logger;
